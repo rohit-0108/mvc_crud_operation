e913a907e753aa53d1d4935194f3f5e74d22db75
Rohit Kolekar <rohitkolekar0108@gmail.com>
2024-07-01 14:50:22 +0530

    mvc_crud

---
+++ Data/MVC_CRUDContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MVC_CRUD.Models;
+
+namespace MVC_CRUD.Data
+{
+    public class MVC_CRUDContext : DbContext
+    {
+        public MVC_CRUDContext (DbContextOptions<MVC_CRUDContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MVC_CRUD.Models.Employee> Employee { get; set; } = default!;
+    }
+}
---
+++ Models/Employee.cs
@@ -0,0 +1,11 @@
+﻿namespace MVC_CRUD.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string Name { get; set; }
+        public DateTime DOB { get; set; }
+        public string Address { get; set; }
+    }
+}
